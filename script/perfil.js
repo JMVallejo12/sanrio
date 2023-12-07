457ed431072fb3e5be87f56bc603ff9227454dd3
정원 <juanma.1010@hotmail.com>
2023-12-07 13:01:45 -0300

    continuado el perfil, y conseguido metodo para acceder a los colores de manera automatica

--- script/perfil.js
+++ script/perfil.js
@@ -5,14 +5,26 @@
 
 const header = document.getElementById("header")
 
-// boton de hello kitty
+// botones
 const btnhello = document.getElementById("btn-hello")
+const btnmelody = document.getElementById("btn-melody")
+const btncinna = document.getElementById("btn-cinnamoroll")
 
 // variable global del color del header
 let colorbg = header.style.backgroundColor.valueOf()
 
+// estilos de mis botones
+const style_hello = getComputedStyle(btnhello)
+const style_melody = getComputedStyle(btnmelody)
+const style_cinna = getComputedStyle(btncinna)
+
 // variables con mis colores
-const hello_kitty = "rgb(195, 1, 1)"
+const hello_kitty = style_hello.color
+const melody = style_melody.color
+const cinna = style_cinna.color
+
+
+// eventos para elegir los personajes
 
 btnhello.addEventListener("click", function(){
 
@@ -21,6 +33,21 @@ btnhello.addEventListener("click", function(){
     
 })
 
+btnmelody.addEventListener("click", function(){
+
+    change_color(melody)
+})
+
+btncinna.addEventListener("click", ()=>{
+
+    change_color(cinna)
+})
+
+
+
+
+
+// funcion que recibe color como parametro
 
 function change_color(color){
 
@@ -28,11 +55,11 @@ function change_color(color){
 
         const blanco = header.style.backgroundColor = "white"
         localStorage.setItem("tema", blanco)
+        
 
     }else{
         header.style.backgroundColor = color
         localStorage.setItem("tema", color)
-
 
     }
     
