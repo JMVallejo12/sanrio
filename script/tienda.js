04690149f7f8f6472a79732e8ba5029573a2e42e
정원 <juanma.1010@hotmail.com>
2023-12-09 11:27:21 -0300

    arreglada la tienda

--- script/tienda.js
+++ script/tienda.js
@@ -13,40 +13,38 @@ const products = [
 
 ]
 
-// haciendo un for para guardar todos los productos
-products.forEach((product)=>{
+// convertir el array de obj
 
-    product_str = JSON.stringify(product)
-    localStorage.setItem(product.name,product_str)
-
-})
-
-Object.keys(localStorage).forEach((product) =>{
-
-    const obj = JSON.parse(localStorage.getItem(product))
+const products_save = localStorage.setItem('productos',JSON.stringify(products))
 
 
-        // generando las cards usando la informacion del localstorage
+// convirtiendo el array a una lista de objetos de vuelta para utilizarlo
 
+const products_obj = JSON.parse(localStorage.getItem('productos'))
+
+   products_obj.forEach(product => {
+    
+         // generando las cards usando la informacion del localstorage
+        
         itemscontainer.innerHTML += `
 
         <div class="card-item" id="shirt-1">
         <!-- foto -->
         <div class="img-product-container">
-            <img src="${obj.img}" alt="Imagen de una camiseta de littletwinstars" class="img-product">
+            <img src="${product.img}" alt="Imagen de una camiseta de littletwinstars" class="img-product">
 
         </div>
 
         <!-- nombre -->
         <div class="name-product-container">
-            <p class="p-product" id="shirt-value">${obj.name}</p>
+            <p class="p-product" id="shirt-value">${product.name}</p>
         </div>
 
         <!-- precio y opciones -->
 
         <div class="price-options">
             <!-- precio -->
-            <div class="price-container">${obj.price}</div>
+            <div class="price-container">${product.price}</div>
 
             <!-- opciones -->
             <div class="options-container">
@@ -56,8 +54,8 @@ Object.keys(localStorage).forEach((product) =>{
                     OPCIONES
                     </button>
                     <ul class="dropdown-menu drop-style">
-                    <li><a class="dropdown-item" href="#">COMPRAR</a></li>
-                    <li><a class="dropdown-item" href="#" id="fav-shirt-1">FAVORITOS</a></li>
+                    <li><a class="dropdown-item" href="#" id=comprar-${product.name}>COMPRAR</a></li>
+                    <li><a class="dropdown-item" href="#" id="fav-${product.name}">FAVORITOS</a></li>
                     </ul>
                 </div>
             </div>
@@ -66,6 +64,48 @@ Object.keys(localStorage).forEach((product) =>{
 
         `
 
+   })
+
+//    comenzando a guadar los articulos en favoritos
+
+// seleccionando todos los elementos del dropdown
+
+
+const dropitems = document.querySelectorAll('.dropdown-item')
+
+
+dropitems.forEach((btn)=>{
+
+    btn.addEventListener("click", function(event){
+
+        event.preventDefault()
+
+        const id = this.id
+
+
+        // verificando si es comprar o favorito
+        const verify = id.startsWith('fav-') ? 
+        (function(){
+
+            return console.log("favorito")
+        }
+
+        ):
+        (
+            function(){
+                return console.log("comprar")
+            }
+        )
+        
+        console.log(verify)
+
+    })
+
 })
+
+
+       
+
+
 
 
