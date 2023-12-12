592dc3de36658129c15c527a84e9ba63a8ecaa1e
정원 <juanma.1010@hotmail.com>
2023-12-11 21:04:04 -0300

    comenzados los filtros de la tienda

--- script/perfil.js
+++ script/perfil.js
@@ -92,8 +92,141 @@ function change_color(color){
         header.style.backgroundColor = color
         localStorage.setItem("tema", color)
 
+
+        // usando toastify para notificar
+        Toastify({
+            text: "Se cambio el tema",
+            duration: 3000,
+            destination: "https://github.com/apvarun/toastify-js",
+            newWindow: true,
+            close: true,
+            gravity: "bottom", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+              background: color
+            },
+            onClick: function(){} // Callback after click
+          }).showToast();
+
     }
     
    
 
 }
+
+
+// leyendo los favs de mi localstorage
+
+const favs_list = JSON.parse(localStorage.getItem('favs'))
+
+// console.log(favs_list)
+
+// leyendo la lista de productos
+
+const product_list = JSON.parse(localStorage.getItem('productos'))
+
+// console.log(product_list)
+
+// id de mi container
+
+const favcontainer = document.getElementById("favs-container")
+
+// encontrando el producto en la lista
+
+
+
+
+    favs_list.forEach(fav => {
+
+        const producto_card = product_list.find(producto => producto.name === fav)
+    
+        console.log(producto_card)
+    
+        if (producto_card){
+    
+            favcontainer.innerHTML += `
+        
+                <div class="card-item" id="shirt-1">
+                <!-- foto -->
+                <div class="img-product-container">
+                    <img src="${producto_card.img}" alt="Imagen de una camiseta de littletwinstars" class="img-product">
+    
+                </div>
+    
+                <!-- nombre -->
+                <div class="name-product-container">
+                    <p class="p-product" id="shirt-value">${producto_card.name}</p>
+                </div>
+    
+                <!-- precio y opciones -->
+    
+                <div class="price-options">
+                    <!-- precio -->
+                    <div class="price-container">${producto_card.price}</div>
+    
+                    <!-- opciones -->
+                    <div class="options-container">
+    
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle btn-options" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            OPCIONES
+                            </button>
+                            <ul class="dropdown-menu drop-style">
+                            <li><a class="dropdown-item" href="#" id="${producto_card.name}">ELIMINAR</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+        
+        `
+    
+    
+        }else{
+            console.log("hola")
+        }
+    
+        
+    
+    })
+
+
+    // eliminando un favorito
+
+const dropdowndelete = document.querySelectorAll('.dropdown-item')
+
+dropdowndelete.forEach((deleteItem) =>{
+
+    deleteItem.addEventListener("click", function(event){
+
+        event.preventDefault()
+
+        const id = this.id
+
+
+        // borrando el elemento de el localstorage
+        const favs_obj = JSON.parse(localStorage.getItem('favs'))
+
+        const index = favs_obj.indexOf(id)
+
+        favs_obj.splice(index, 1)
+
+        localStorage.setItem('favs',JSON.stringify(favs_obj))
+
+
+        // poner un toastify para que notifique al usuario de que se borro y que recargue la pagina
+
+
+
+
+
+    })
+})
+
+
+
+
+
+
+    
+
--- script/tienda.js
+++ script/tienda.js
@@ -1,3 +1,5 @@
+let favs = JSON.parse(localStorage.getItem('favs')) || []
+
 // trayendo la id de mi container
 
 const itemscontainer = document.getElementById("items-container")
@@ -10,8 +12,10 @@ const products = [
     {name:"LittleTwinStars Sanrio Original Graphic Tee", type: "shirt", price: "22$", img: "../assets/shirt-2.jpg"},
     {name:"Hello Kitty Cowboy Hat Tee", type: "shirt", price: "15$", img: "../assets/shirt-3.webp"},
     {name:"Hello Kitty Wild West Tee", type: "shirt", price: "19.99$", img: "../assets/shirt-4.webp"},
+    {name:"Hello Kitty x Dumbgood Butterfly Glitter Sweatpants", type: "pants", price: "90$", img:"../assets/pants-1.webp"},
 
 ]
+
 
 // convertir el array de obj
 
@@ -54,8 +58,7 @@ const products_obj = JSON.parse(localStorage.getItem('productos'))
                     OPCIONES
                     </button>
                     <ul class="dropdown-menu drop-style">
-                    <li><a class="dropdown-item" href="#" id=comprar-${product.name}>COMPRAR</a></li>
-                    <li><a class="dropdown-item" href="#" id="fav-${product.name}">FAVORITOS</a></li>
+                    <li><a class="dropdown-item" href="#" id="${product.name}">FAVORITOS</a></li>
                     </ul>
                 </div>
             </div>
@@ -67,6 +70,9 @@ const products_obj = JSON.parse(localStorage.getItem('productos'))
    })
 
 //    comenzando a guadar los articulos en favoritos
+
+
+
 
 // seleccionando todos los elementos del dropdown
 
@@ -82,27 +88,83 @@ dropitems.forEach((btn)=>{
 
         const id = this.id
 
+        // comprobando si ese fav ya esta en la lista, para evitar que se repita
+        if (favs.includes(id)){
 
-        // verificando si es comprar o favorito
-        const verify = id.startsWith('fav-') ? 
-        (function(){
+            // arreglar con un sweet alert
+            // usando toastify para notificar
+            Toastify({
+                text: "Ya esta en la lista",
+                duration: 3000,
+                destination: "https://github.com/apvarun/toastify-js",
+                newWindow: true,
+                close: true,
+                gravity: "bottom", // `top` or `bottom`
+                position: "right", // `left`, `center` or `right`
+                stopOnFocus: true, // Prevents dismissing of toast on hover
+                style: {
+                background: localStorage.getItem('tema')
+                },
+                onClick: function(){} // Callback after click
+            }).showToast();
 
-            return console.log("favorito")
+        }else{
+
+                // guardando el favorito en el localstorage
+            favs.push(id)
+            console.log(favs)
+
+            favs_storage = localStorage.setItem('favs',JSON.stringify(favs))
+            
+            Toastify({
+                text: "Se agrego a la lista",
+                duration: 3000,
+                destination: "https://github.com/apvarun/toastify-js",
+                newWindow: true,
+                close: true,
+                gravity: "bottom", // `top` or `bottom`
+                position: "right", // `left`, `center` or `right`
+                stopOnFocus: true, // Prevents dismissing of toast on hover
+                style: {
+                background: localStorage.getItem('tema')
+                },
+                onClick: function(){} // Callback after click
+            }).showToast();
+
         }
 
-        ):
-        (
-            function(){
-                return console.log("comprar")
-            }
-        )
-        
-        console.log(verify)
+       
 
     })
 
 })
 
+
+// seleccionamos todos los checkbox para hacer un filtro
+
+const check = document.querySelectorAll('.checkbox')
+
+// se recorren todos los checkbox con un foreach
+
+check.forEach((check)=>{
+
+    // se usa el evento change para verificar un cambio del estado del checkbox
+    check.addEventListener('change',()=>{
+
+        // utilizamos el atributo data filter de nuestros checkbox para obtener el producto que se quiere filtrar
+        const filter_product = check.getAttribute('data-filter')
+        console.log(filter_product)
+
+        // mostrando los articulos
+
+        itemscontainer.innerHTML = ""
+
+        products_filter = JSON.parse(localStorage.getItem('productos'))
+        console.log(products_filter)
+        
+    })
+
+})
 
        
 
